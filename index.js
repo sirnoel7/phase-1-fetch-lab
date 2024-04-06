301b6f421ce14df13baa48b106ff74efb3c86f7d
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-04-06 15:27:13 +0300

    books

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books") 
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json)); 
 }
 
 function renderBooks(books) {
@@ -13,5 +14,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
+    .catch(error => console.error('Error fetching books:', error)); 
 });
